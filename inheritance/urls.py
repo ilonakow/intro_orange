b97f21e27ee3cc1a73d15238fc4262605b339c65
Ilona Kowalewska <ilona.kowalewska112@gmail.com>
2023-05-06 13:13:25 +0200

    dziedziczenie w dtl

--- inheritance/urls.py
+++ inheritance/urls.py
@@ -5,4 +5,5 @@ from inheritance import views
 app_name = 'inheritance'
 urlpatterns = [
     path('first/', views.first_view, name='first_name'),
+    path('second/', views.second_view, name='second_name'),
 ]

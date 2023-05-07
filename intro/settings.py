6456b0332dcacf157d890637fd2109e65b0b17af
Ilona Kowalewska <ilona.kowalewska112@gmail.com>
2023-05-07 16:24:30 +0200

    U part 1

--- intro/settings.py
+++ intro/settings.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
     'form_app2.apps.FormApp2Config',
     'form_app3.apps.FormApp3Config',
     'form_app4.apps.FormApp4Config',
-  'form_app5.apps.FormApp5Config',
+    'form_app5.apps.FormApp5Config',
+    'crud_app.apps.CrudAppConfig',
 
 
 ]

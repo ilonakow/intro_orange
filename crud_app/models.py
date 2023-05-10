4921d0b4ab54c729775bacfa4496e198701d53e2
Ilona Kowalewska <ilona.kowalewska112@gmail.com>
2023-05-10 18:53:20 +0200

    m2m

--- crud_app/models.py
+++ crud_app/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-from django.db import models
-
 
 # Create your models here.
 class Task(models.Model):
---
+++ relations_app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+# One-to-one (O2O)
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    capital = models.OneToOneField('Capital', on_delete=models.CASCADE)
+
+
+class Capital(models.Model):
+    name = models.CharField(max_length=100)
+
+
+# One-to-many (O2M)
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    # framework_set =  # automatycznie dorobiony przez django menadżer powiązany
+
+class Framework(models.Model):
+    name = models.CharField(max_length=100)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE)
+    # language_id = ...  # automatycznie dorobione przez django pole
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+    # movie_set automatycznie dorobiony przez django menager powiazany
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    actors = models.ManyToManyField('Actor') # to tez jest menager powiązany z tabela Actor

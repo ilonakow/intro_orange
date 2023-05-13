97afffb6ecc9652da1ab9d056339875427375946
Ilona Kowalewska <ilona.kowalewska112@gmail.com>
2023-05-13 11:43:22 +0200

    django model form

--- message_app/forms.py
+++ message_app/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
+from message_app.models import Message
 
+# django form
 class MessageForm(forms.Form):
     CHOICES = (
         ('question', 'pytanie'),
@@ -10,6 +12,41 @@ class MessageForm(forms.Form):
     email = forms.EmailField(label='Email')
     category = forms.ChoiceField(choices=CHOICES, label="Kategoria")
     subject = forms.CharField(label="Tytuł")
-    body = forms.CharField(label="Treść")
-    fav_date = forms.DateField(label="Ulubiona data")
-    fav_time = forms.TimeField(label="Ulubiony czas")()
+    body = forms.CharField(
+        label="Treść",
+        widget=forms.Textarea(attrs={'cols': 40, 'rows': 10})
+    )
+    fav_date = forms.DateField(
+        label="Ulubiona data",
+        widget=forms.NumberInput(attrs={'type': 'date'})
+    )
+    fav_time = forms.TimeField(
+        label="Ulubiony godzina",
+        widget=forms.NumberInput(attrs={'type': 'time'})
+    )
+
+
+# model form
+class MessageModelForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = '__all__'
+        # fields = [
+        #     'email',
+        #     'category',
+        #     ...
+        # ]  # ew. exclude
+
+        labels = {
+            'email': "Email",
+            'category': "Kategoria",
+            'subject': 'Tytuł',
+            'body': 'Treść',
+            'fav_date': 'Ulubiona data',
+            'fav_time': 'Ulubiona godzina'
+        }
+        widgets = {
+            'body': forms.Textarea(attrs={'cols': 40, 'rows': 10}),
+            'fav_date': forms.NumberInput(attrs={'type': 'date'}),
+            'fav_time': forms.NumberInput(attrs={'type': 'time'})
+        }
--- message_app/views.py
+++ message_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from message_app.models import Message
-from message_app.forms import MessageForm
+from message_app.forms import MessageForm, MessageModelForm
 
 
 # html form
@@ -14,6 +14,8 @@ def contact1(request):
         body = data.get('body')
         fav_date = data.get('fav_date')
         fav_time = data.get('fav_time')
+
+        # walidacja na backendzie (my tutaj przepuszczamy wszystko)
 
         Message.objects.create(
             email=email,
@@ -31,13 +33,75 @@ def contact1(request):
     )
 
 
+# django form
 def contact2(request):
-    form = MessageForm()
+    if request.method == "POST":
+        form = MessageForm(request.POST)  # bound form
 
-    return render(
-        request,
-        'message_app/contact2.html',
-        context={
-            'form': form,
-        }
-    )
+        if form.is_valid():
+            data = form.cleaned_data
+            email = data.get('email')
+            category = data.get('category')
+            subject = data.get('subject')
+            body = data.get('body')
+            fav_date = data.get('fav_date')
+            fav_time = data.get('fav_time')
+
+            Message.objects.create(
+                email=email,
+                category=category,
+                subject=subject,
+                body=body,
+                fav_date=fav_date,
+                fav_time=fav_time,
+            )
+
+            return redirect('message_app:contact2')
+        else:
+            return render(
+                request,
+                'message_app/contact2.html',
+                context={
+                    'form': form,
+                }
+            )
+
+    elif request.method == "GET":
+        form = MessageForm()  # unbound django form
+
+        return render(
+            request,
+            'message_app/contact2.html',
+            context={
+                'form': form,
+            }
+        )
+
+
+# model form
+def contact3(request):
+    if request.method == "POST":
+        form = MessageModelForm(request.POST)  # bound model form
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('message_app:contact2')
+        else:
+            return render(
+                request,
+                'message_app/contact3.html',
+                context={
+                    'form': form,
+                }
+            )
+
+    if request.method == "GET":
+        form = MessageModelForm()  # unbound model form
+        return render(
+            request,
+            'message_app/contact3.html',
+            context = {
+                'form': form,
+            }
+        )
